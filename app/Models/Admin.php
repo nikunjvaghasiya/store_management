9fd52d9283df615fef21db17ec2ed0eb2b354330
Ravi Vachhani <ravi.versatiletechno@gmail.com>
2021-07-12 13:05:17 +0530

    first commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'firstname', 'midname', 'lastname', 'email', 'address', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+   }
